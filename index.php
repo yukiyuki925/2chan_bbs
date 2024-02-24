92ab3e1e46291a2e1e0917cf2ff35ff7956b9201
yukiyuki925 <yuki.work1999@gmail.com>
2024-02-24 18:21:26 +0900

    attack escape

--- index.php
+++ index.php
@@ -1,19 +1,28 @@
 <?php
 
+// データベースに接続
 include_once("./app/database/connect.php");
 
+// エラーを連想配列で格納
 $error = array();
 
+// 'submitButton'が送信されたら->submitボタンが押されたら
 if (isset($_POST['submitButton'])) {
 
   // 名前入力チェック
   if(empty($_POST["username"])) {
     $error["username"] = 'お名前を入力してください';
+  } else {
+    // エスケープ処理
+    $escaped['username'] = htmlspecialchars($_POST['username'], ENT_QUOTES, 'UTF-8');
   }
 
   // コメント入力チェック
   if(empty($_POST["body"])) {
     $error["body"] = 'コメントを入力してください';
+  } else {
+    // エスケープ処理
+    $escaped['body'] = htmlspecialchars($_POST['body'], ENT_QUOTES, 'UTF-8');
   }
 
   if(empty($error)) {
@@ -23,8 +32,8 @@ if (isset($_POST['submitButton'])) {
     $statement = $pdo->prepare($sql);
 
     // 値をセットする
-    $statement->bindParam(":username", $_POST['username'], PDO::PARAM_STR);
-    $statement->bindParam(":body", $_POST['body'], PDO::PARAM_STR);
+    $statement->bindParam(":username", $escaped['username'], PDO::PARAM_STR);
+    $statement->bindParam(":body", $escaped['body'], PDO::PARAM_STR);
     $statement->bindParam(":post_date", $post_date, PDO::PARAM_STR);
 
     $statement->execute();
